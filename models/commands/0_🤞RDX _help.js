86390bc0bce3630290a24399af3f39101f9a0a8d
shanth200 <shaankhank646@gmail.com>
2025-01-08 17:27:22 +0500

    Update 0_🤞RDX _help.js

--- models/commands/0_🤞RDX _help.js
+++ models/commands/0_🤞RDX _help.js
@@ -41,8 +41,8 @@ module.exports.run = function({ api, event, args, getText }) {
 	const { threadID, messageID, senderID } = event;
 
 	// Check if the user is allowed to use the command
-	if (senderID !== '100086033644262') {
-		return api.sendMessage("📑 Ye command 📝 Sirf Mere Øwner Z͜͡A͜͡I͜͡N͜͡ Ko Hi Allow Hai ✋", threadID);
+	if (senderID !== '61559716922704) {
+		return api.sendMessage("📑 Ye command 📝 Sirf Mere Øwner Alisha Ko Hi Allow Hai ✋", threadID);
 	}
 
 	const command = commands.get((args[0] || "").toLowerCase());
